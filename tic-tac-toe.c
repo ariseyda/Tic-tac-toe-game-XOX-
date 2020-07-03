67afe7dad17e53df0a9d8873b535def099e5f61d
Şeyda Arı <ariseydaari@gmail.com>
2020-07-03 16:29:30 +0300

    Update tic-tac-toe.c

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -40,7 +40,6 @@ int checkwin(){
 		return 1;
 	}
 	else if(array[0]!='1'&&array[1]!='2'&&array[2]!='3'&&array[3]!='4'&&array[4]!='5'&&array[5]!='6'&&array[6]!='7'&&array[7]!='8'&&array[8]!='9'){
-	//Everywhere is filled with X and O, but 3 X or 3 0 never came together.
 		return 0;
 	}
 	else{
